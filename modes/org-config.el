ece0f54a4e2a593a945c3855ab159d67b901d6d1
James Cash <james.nvc@gmail.com>
2008-07-20 20:51:13 -0400

    removing keybindings from org-confg, also changing remember templates

--- modes/org-config.el
+++ modes/org-config.el
@@ -1,28 +1,9 @@
-(define-key mode-specific-map [?a] 'org-agenda)
-
-(eval-after-load "org"
-  '(progn
-     (define-prefix-command 'org-todo-state-map)
-     (define-key org-mode-map (kbd "C-c x") 'org-todo-state-map)
-     (define-key org-todo-state-map "x"
-       #'(lambda nil (interactive) (org-todo "CANCELLED")))
-     (define-key org-todo-state-map "d"
-       #'(lambda nil (interactive) (org-todo "DONE")))
-     (define-key org-todo-state-map "f"
-       #'(lambda nil (interactive) (org-todo "DEFERRED")))
-     (define-key org-todo-state-map "l"
-       #'(lambda nil (interactive) (org-todo "DELEGATED")))
-     (define-key org-todo-state-map "s"
-       #'(lambda nil (interactive) (org-todo "STARTED")))
-     (define-key org-todo-state-map "w"
-       #'(lambda nil (interactive) (org-todo "WAITING")))
-;;;      (define-key org-agenda-mode-map "\C-n" 'next-line)
-;;;      (define-key org-agenda-keymap "\C-n" 'next-line)
-;;;      (define-key org-agenda-mode-map "\C-p" 'previous-line)
-;;;      (define-key org-agenda-keymap "\C-p" 'previous-line)))
-))
-
-
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
 
-(define-key global-map [(control meta ?r)] 'remember)
+(setq org-remember-templates
+      '(("Todo"  ?t "** TODO %?\n  %u" "~/todo.org"  "Tasks")
+        ("Notes" ?n "* %u %?"         "~/notes.org" "Notes")
+        ("Appts" ?a "** APPT %?\n%^T\n%i\n" "~/todo.org" "Appointments")
+        ("Book"  ?b "*** TODO %?\n  %u" "~/todo.org" "Books"))
+      )
+
