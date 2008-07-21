bf9035967959af9d54c737d90f94688e76c062bb
James Cash <james.nvc@gmail.com>
2008-07-21 02:14:18 -0400

    Stop using vc-git, as it doesn't really seem to be working very well

--- modes/mode-configs.el
+++ modes/mode-configs.el
@@ -64,8 +64,8 @@
 (display-battery-mode 1)
 (setq battery-update-interval 30)
 
-(require 'vc-git)
-(when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
+;; (require 'vc-git)
+;; (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
 (require 'git)
 (autoload 'git-blame-mode "git-blame" "Minor mode for incremental blame for Git." t)
 
