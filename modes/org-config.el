f1b4a98ad9e5abda52d6c26e36ea49602ff57318
James Cash <james.nvc@gmail.com>
2008-08-07 23:55:19 -0400

    Moving stuff from org-config to customize

--- modes/org-config.el
+++ modes/org-config.el
@@ -1,14 +1,15 @@
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
 
-(setq org-remember-templates
-      '(("Todo"  ?t "** TODO %?\n  %u" "~/todo.org"  "Tasks")
-        ("Notes" ?n "* %u %?"         "~/notes.org" "Notes")
-        ("Appts" ?a "** APPT %?\n   %^T\n%i\n" "~/todo.org" "Appointments")
-        ("Book"  ?b "*** TODO %?\n  %u" "~/todo.org" "Books"))
-      )
+;; (setq org-remember-templates
+;;       '(("Todo"  ?t "** TODO %?\n  %u" "todo.org"  "Tasks")
+;;         ("Notes" ?n "* %u %?"         "notes.org" "Notes")
+;;         ("Appts" ?a "** APPT %?\n   %^T\n%i\n" "todo.org" "Appointments")
+;;         ("Book"  ?b "*** TODO %?\n  %u" "todo.org" "Books"))
+;;       )
+;; "\n* %^{Book Title} %t :READING: \n%[l:/booktemp.txt]\n" 
 
-(setq org-link-abbrev-alist
-      '(("wiki" . "http://en.wikipedia.org/wiki/")))
+;; (setq org-link-abbrev-alist
+;;       '(("wiki" . "http://en.wikipedia.org/wiki/")))
 
 (setq org-agenda-include-diary t)
 
