72ff2742a0743c93341263f4f5443612490fc70d
James Cash <james.nvc@gmail.com>
2008-10-27 22:45:21 -0400

    Moving some stuff in clojure-config around

--- modes/clojure-config.el
+++ modes/clojure-config.el
@@ -2,15 +2,13 @@
 ;;;; Clojure mode
 ;;;;
 (require 'clojure-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
  
 ;;;;
 ;;;; Setup slime
 ;;;;
 ;; (require 'slime-autoloads)
 ;; (slime-setup '(slime-scratch slime-editing-commands))
-
-(setf slime-lisp-implementations `((sbcl ,(concat "/home/james/bin/sbcl --core /home/james/lib/sbcl/sbcl.core"))))
+(setf slime-lisp-implementations `((sbcl ("/home/james/bin/sbcl --core /home/james/lib/sbcl/sbcl.core" ""))))
 (setq swank-clojure-jar-path "/home/james/src/clojure/target/clojure-lang-1.0-SNAPSHOT.jar")
 (require 'swank-clojure-autoload)
 
