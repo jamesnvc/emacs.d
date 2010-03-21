1ff9e344775db996ae4cfd165330f6dac2235715
James N. V. Cash <james.nvc@gmail.com>
2010-03-21 09:34:20 -0400

    Making LaTeX mode use XeTex by default

--- custom/customizations.el
+++ custom/customizations.el
@@ -83,6 +83,15 @@
 
 (require 'ecb)
 
+;; Latex
+(eval-after-load "tex"
+  (progn
+    (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+    (setq TeX-command-default "XeLaTeX")
+    (setq TeX-save-query nil)
+    (setq TeX-show-compilation t)))
+
+
 ;; Twitter
 (autoload 'twitter-get-friends-timeline "twitter" nil t)
 (autoload 'twitter-status-edit "twitter" nil t)
